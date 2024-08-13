eb9d8b60d7740f57f4049ee7798cb083b7be7097
rakibhossaingalib <rakib@primetechbd.com>
2024-08-13 22:47:06 +0600

    inteface,repository and manager implemented for solid design pattern

---
+++ Src/Services/Catelog.API/Interface/IRepository/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Catelog.API.Models;
+using MongoRepo.Interfaces.Repository;
+
+namespace Catelog.API.Interface.IRepository
+{
+    public interface IProductRepository:ICommonRepository<Product>
+    {
+    }
+}
---
+++ Src/Services/Catelog.API/Repository/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Catelog.API.Context;
+using Catelog.API.Interface.IRepository;
+using Catelog.API.Models;
+using MongoRepo.Context;
+using MongoRepo.Repository;
+using System.ComponentModel;
+
+namespace Catelog.API.Repository
+{
+    public class ProductRepository : CommonRepository<Product>, IProductRepository
+    {
+        public ProductRepository() : base(new CatalogDbContext())
+        {
+        }
+    }
+}
