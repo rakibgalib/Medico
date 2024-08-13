b3bcb339a3992b92ad738bab87989b25eb4b20d8
rakibhossaingalib <rakib@primetechbd.com>
2024-08-13 08:14:31 +0600

    context create with model product

---
+++ Src/Services/Catelog.API/Context/CatalogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MongoRepo.Context;
+
+namespace Catelog.API.Context
+{
+    public class CatalogDbContext : ApplicationDbContext
+    {
+        static IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true).Build();
+        //static string connectionString = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true,true).Build().GetConnectionString("Catalog.API");
+        static string connectionString = configuration.GetConnectionString("Catalog.API");
+        static string databaseName = configuration.GetValue<string>("DatabaseName");
+        public CatalogDbContext() : base(connectionString, databaseName)
+        {
+        }
+    }
+}
---
+++ Src/Services/Catelog.API/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catelog.API.Models
+{
+    public class Product
+    {
+        [BsonId]
+       public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Summery { get; set; }
+        public int ImageFile { get; set; }
+    }
+}
